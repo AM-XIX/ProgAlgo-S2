392d2e0bed4ba31b01b1991fc0344c817cf1133f
AM-XIX <am.lannaud@gmail.com>
2024-04-26 09:36:09 +0200

    S2-TD4 complété

--- S2-TD4/main.cpp
+++ S2-TD4/main.cpp
@@ -2,11 +2,36 @@
 #include <vector>
 #include <cstdlib>
 #include <numeric>
+#include <sstream>
+#include "main.hpp"
+
+// ===== Exercice 2 =====
+// ———————— 02-01 —————————
+int countLetters(const std::string& sentence) {
+    auto it = std::find(sentence.begin(), sentence.end(), ' ');
+    return std::distance(sentence.begin(), it);
+}
+
+// ———————— 02-02 —————————
+std::vector<std::string> splitSentence(const std::string& sentence) {
+    std::vector<std::string> words;
+    std::string word;
+    std::istringstream iss(sentence);
+    
+    while (iss >> word) {
+        words.push_back(word);
+    }
+    
+    return words;
+}
+
+ // ===== Exercice 3 =====
+bool isPalindrome(const std::string& str) {
+    return std::equal(std::begin(str), std::end(str), std::rbegin(str));
+}
 
 int main() {
-
-    // ===== Exercice 1 (Vector and Algorithm) =====
-    
+    // ===== Exercice 1 =====
     // ———————— 01-01 —————————
     std::vector<int> v(10);
     // -------- Remplissage du vecteur
@@ -21,17 +46,17 @@ int main() {
         std::sort(v.begin(), v.end());
 
         // ———————— 01-06 —————————
-        std::cout << std::accumulate(v.begin(), it, 0) << std::endl;
+        // std::cout << std::accumulate(v.begin(), it, 0) << std::endl;
 
         // comm 06 et décom ça pour avoir 01-02 seulement :
         // std::cout << *it << " ";
     }
 
     // ———————— 01-03 —————————
-    auto it { std::find(v.begin(), v.end(), 69) };
+    auto it { std::find(v.begin(), v.end(), 80) };
     if (it != v.end())
     {
-        // Essai bonus parce que j'avais mal compris la consigne :
+        // Essai bonus parce que j'avais mal compris la consigne bruh :
         // std::cout << "Element found at position " << it - v.begin() << std::endl;
     }
     else
@@ -41,5 +66,17 @@ int main() {
 
     // ———————— 01-04 —————————
     std::cout << std::count(v.begin(), v.end(), 41) << std::endl;
+    
+    // ———————— 02-01 —————————
+    std::cout << countLetters("Trololol") << std::endl;
+    
+    // ———————— 02-02 —————————
+    std::vector<std::string> words = splitSentence("Tololo lolol ololol");
+    for (const std::string& word : words) {
+        std::cout << word << std::endl;
+    }
+    std::cout << std::endl;
 
+    // ===== Exercice 3 =====
+    std::cout << isPalindrome("kayak") << std::endl;
 }
---
+++ S2-TD4/main.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+// ———————— 02-01 —————————
+auto const is_space = [](char letter){ return letter == ' '; };
+
+// ———————— 02-02 —————————
+std::vector<std::string> split_string(std::string const& str);
+
+// ———————— 03 —————————
+bool is_palindrome(std::string const& str);
